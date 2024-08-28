f8117ab1ca9a124b97705e1ee74cace28c53680c
Edwin Ortiz <edarcode@gmail.com>
2024-08-28 01:30:56 -0500

    maquetado del form sign up

--- skillup-client/src/pages/SignUp/SignUp.jsx
+++ skillup-client/src/pages/SignUp/SignUp.jsx
@@ -1,3 +1,16 @@
+import Btn from "./Btn/Btn.jsx";
+import css from "./css.module.css";
+import InputText from "./InputText/InputText.jsx";
+import LinkTo from "./LinkTo/LinkTo.jsx";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <form className={css.form}>
+      <InputText title="Email" placeholder="Value" />
+      <InputText title="Password" placeholder="Value" />
+      <LinkTo to="/">Forgot password?</LinkTo>
+      <Btn>Sign up</Btn>
+      <LinkTo to="/">Sign in</LinkTo>
+    </form>
+  );
 }
