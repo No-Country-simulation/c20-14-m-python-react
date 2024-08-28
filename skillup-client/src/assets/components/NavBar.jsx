a1daab917c1c6410b2bd1da202f328b72d70d7ba
Tamara Contreras <102377583+tamaracontreras@users.noreply.github.com>
2024-08-27 23:14:29 -0400

    Revert "Componente NavBar"

--- skillup-client/src/assets/components/NavBar.jsx
+++ skillup-client/src/assets/components/NavBar.jsx
@@ -1,38 +1,33 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-// import logo1 from '../images/logo1.svg';
-import logo2 from '../images/logo3.svg';
-import '../styles/NavBar.css';
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
 
-function NavBar() {
+function Navbar() {
   return (
-    <Navbar bg="light" variant="ligth" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-        <img 
-        src={logo2} 
-        width="100"
-        height="100"
-        className="d-inline-block align-top" alt="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
-            <Nav.Link href="#Recomendaciones"><Button variant="light" size="md">Recomendaciones</Button>{' '}</Nav.Link>
-            <Nav.Link href="#CursosDisponibles"><Button variant="light" size="md">Cursos Disponibles</Button>{' '}</Nav.Link>
-            </Nav>
-            <Nav className="ms-auto">
-            <Nav.Link href="#Contact"><Button variant="light" size="sm">Contact</Button>{' '}</Nav.Link>
-            <Nav.Link href="#Sign_In"><Button variant="light" size="sm"> Sign In</Button>{' '}</Nav.Link>
-            <Nav.Link href="#Register"><Button variant="dark" size="sm"> Register</Button>{' '}</Nav.Link>
-            </Nav>
-           
-          
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Mi Proyecto</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Características</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Precios</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Deshabilitado</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
-export default NavBar;
+export default Navbar;
