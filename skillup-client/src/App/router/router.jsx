31790267ca8a967c76b18d3486d1a5910a5fbb99
Edwin Ortiz <edarcode@gmail.com>
2024-08-29 07:53:13 -0500

    cambiando nombre de ErrPage

--- skillup-client/src/App/router/router.jsx
+++ skillup-client/src/App/router/router.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HOME, LOGIN, SIGN_UP } from "./childres.jsx";
-import ErrPage from "../views/ErrPage/ErrPage.jsx";
 import App from "../App.jsx";
+import NotFound404 from "../views/NotFound404/NotFound404.jsx";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
-		errorElement: <ErrPage />,
+		errorElement: <NotFound404 />,
 		children: [HOME, SIGN_UP, LOGIN]
 	}
 ]);
--- skillup-client/src/App/views/ErrPage/ErrPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function ErrPage() {
-	return <div>err page</div>;
-}
