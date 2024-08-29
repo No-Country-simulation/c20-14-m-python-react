deac7a1e960cad5cd65e3aa75bd3d068759709b7
Edwin Ortiz <edarcode@gmail.com>
2024-08-29 13:35:35 -0500

    fix

--- skillup-client/src/App/router/router.jsx
+++ skillup-client/src/App/router/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { HOME, LOGIN, SIGN_UP } from "./childres.jsx";
+import { SIGN_UP } from "./childres.jsx";
 import App from "../App.jsx";
 import NotFound404 from "../views/NotFound404/NotFound404.jsx";
 
@@ -8,6 +8,6 @@ export const router = createBrowserRouter([
 		path: "/",
 		element: <App />,
 		errorElement: <NotFound404 />,
-		children: [HOME, SIGN_UP, LOGIN]
+		children: [SIGN_UP]
 	}
 ]);
