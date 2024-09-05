0a06c7ad6aa2d4a1fb2c56f7b10ded5bc1a448f1
Jair <92953233+JairRoMe@users.noreply.github.com>
2024-09-05 10:55:33 -0600

    Creación de endpoint que activa la cuenta de usuario asociada al url enviado al usuario
    El endpoint recibe por medio de la url el  id del usuario creado y no verificado y el token generado por el servidor, verifica que el token sea válido, de ser así se actualiza el campo en la base de datos que activa a el usuario y se devuelve un JSON con status 200, de lo contrario no se realizan cambios en la base de datos y se devuelve un JSON con status 400

--- skillup-server/skillup/urls.py
+++ skillup-server/skillup/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import activate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('activate/<uidb64>/<token>', activate, name='activate'),
 ]
