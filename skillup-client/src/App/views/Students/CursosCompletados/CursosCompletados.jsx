7311a729d64778de518fde0f11f2f02aa9e4f112
Tam <tacs.laboral@gmail.com>
2024-09-05 17:03:57 -0400

    actualización de diseño desktop de progreso de cursos

--- skillup-client/src/App/views/Students/CursosCompletados/CursosCompletados.jsx
+++ skillup-client/src/App/views/Students/CursosCompletados/CursosCompletados.jsx
@@ -9,13 +9,11 @@ import {
 } from "react-bootstrap";
 import DataEjemplo from "./DataEjemplo";
 import "../styles/CursosCompletados.css";
-// import sql.webp from "../img/sql.webp";
 
 function CursosCompletados() {
-	// Verificar si DataEjemplo está vacío
 	if (DataEjemplo.length === 0) {
 		return (
-			<Container fluid className="cursocomp">
+			<Container fluid className="cardContainer ">
 				<Row>
 					<Col>
 						<h2>Cursos Completados</h2>
@@ -23,12 +21,12 @@ function CursosCompletados() {
 				</Row>
 				<Row>
 					<Col xs={12} md={6} lg={4}>
-						<Card className="mb-4" style={{ width: "18rem" }}>
-							<Card.Body className="text-center">
+						<Card className="mb-4 cardsize">
+							<Card.Body className="text-center mt-3">
 								<Card.Title>No se ha inscrito a ningún curso aún</Card.Title>
 								<Button
 									className="mt-3"
-									variant="primary"
+									variant="dark"
 									href="/cursos-disponibles" // Cambia esta URL a la ruta correcta
 								>
 									Ver Cursos Disponibles
@@ -42,7 +40,7 @@ function CursosCompletados() {
 	}
 
 	return (
-		<Container fluid className="cursocomp">
+		<Container fluid className="cardContainer">
 			<Row>
 				<Col>
 					<h2>Cursos Completados</h2>
@@ -51,32 +49,34 @@ function CursosCompletados() {
 			<Row>
 				{DataEjemplo.map((course, index) => (
 					<Col xs={12} md={6} lg={4} key={index}>
-						<Card className="mb-4" style={{ width: "18rem" }}>
-							<Card.Img
-								variant="top"
-								src={course.imageUrl}
-								alt={`Imagen del curso ${course.name}`}
-							/>
+						<Card className="mb-4">
+							<div className="cardImg">
+								<Card.Img
+									variant="top"
+									src={course.imageUrl}
+									alt={`Imagen del curso ${course.name}`}
+									className="card-img-fixed"
+								/>
+							</div>
 							<ProgressBar
 								className="rounded-0 mb-2 progress-bar-black"
 								now={course.progreso}
 								label={`${course.progreso}%`}
 							/>
 							<Card.Body>
-								<Badge className="text-center" text="light" bg="dark">
+								<Badge className="text-center fs-6 mb-4" text="light" bg="dark">
 									{course.name}
 								</Badge>
 								<Card.Title className="fs-6">
 									Inicio: {course.inicio}
 								</Card.Title>
-								<Card.Title className="fs-6">
+								<Card.Title className="fs-6 mb-4">
 									Finalización: {course.date}
 								</Card.Title>
-
 								{/* Mostrar el botón solo si el progreso es 100% */}
 								{course.progreso === 100 && (
 									<Button
-										className="d-flex justify-content-center btn-black"
+										className="d-flex justify-content-center mx-auto btn-black w-50 "
 										href={course.certificateUrl}
 										target="_blank"
 										rel="noopener noreferrer"
