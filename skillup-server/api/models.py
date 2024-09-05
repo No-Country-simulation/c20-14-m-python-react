cbbb521dc605a54165cbd14890190772811658fd
Fernando <fernandostarlin1012@gmail.com>
2024-09-05 10:29:56 -0400

    Aplicando migraciones

--- skillup-server/api/models.py
+++ skillup-server/api/models.py
@@ -75,7 +75,7 @@ class Category(SoftDeleteModel):
 class Course(SoftDeleteModel):
     title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField()
-    category = models.ManyToManyField('Category', on_delete=models.DO_NOTHING, related_name='courses')
+    category = models.ManyToManyField('Category', related_name='courses')
     duration = models.PositiveIntegerField()
     date_created = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
