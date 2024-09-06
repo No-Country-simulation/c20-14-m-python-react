858785a3e6547e0f126b4503da7b065f06e13caf
Fernando <fernandostarlin1012@gmail.com>
2024-09-06 18:14:41 -0400

    Revert "Fixing issues associates with the endpoint POST /register"
    This reverts commit ae8584bc4ea668de97d7ad7ea8179f0b64516ae1.

--- skillup-server/api/views.py
+++ skillup-server/api/views.py
@@ -6,32 +6,31 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 from django.views import View
 
 from skillup.auth import generate_jwt_token, refresh_token_view
-
 
 @method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(View):
 
     def post(self, request):
         data = json.loads(request.body)
-        email = data.get('email')
-        password = data.get('password')
-
+        email = data.get ('email')
+        password = data.get ('password')
+        username = data.get ('username')
         if User.objects.filter(email=email):
-            return JsonResponse({'error': 'Este e-mail ya esta en uso'}, status=400)
-
-            # Next to do, create return error for invalid JSON
-            # '''JSONDecodeError
-            # ValidationError
-            # except Exception'''
-
-        user = User.objects.create(username=email, email=email, password=password)
+            return JsonResponse ({'error': 'Este e-mail ya esta en uso'}, status=400)
+        
+            #Next to do, create return error for invalid JSON 
+            '''JSONDecodeError
+            ValidationError
+            except Exception''' 
+        
+        user = User.objects.create(username=username, email=email, password=password)
         Profile.objects.create(user=user)
 
         token = generate_jwt_token(user.id)
-        return JsonResponse({'message': 'Usuario registrado existosamente', 'token': token}, status=201)
+        return JsonResponse ({'message': 'Usuario registrado existosamente', 'token': token}, status=201)
 
     
