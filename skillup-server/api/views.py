f2d359df3cb94669b2595b8ae23b64feae1b11c0
Jair <92953233+JairRoMe@users.noreply.github.com>
2024-09-05 19:04:03 -0600

    Correción de nomenclatura para el endpoint de activación de usuario

--- skillup-server/api/views.py
+++ skillup-server/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .utils.verification import validateToken
+from .utils.verification import validate_token
 
 
 def activate(request, uidb64, token):
-    user = validateToken(uidb64, token)
+    user = validate_token(uidb64, token)
     if user is not None:
         user.is_active = True
         user.save()
