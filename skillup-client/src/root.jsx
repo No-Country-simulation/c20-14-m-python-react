9dfe70a8e838815005bc2243a2c153145032a33f
Edwin Ortiz <edarcode@gmail.com>
2024-09-10 03:10:41 -0500

    quitando tanStackQuery

--- skillup-client/src/root.jsx
+++ skillup-client/src/root.jsx
@@ -1,16 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { RouterProvider } from "react-router-dom";
 import "./root.css";
-import { router } from "./App/router/router.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")).render(
-	<QueryClientProvider client={queryClient}>
-		<RouterProvider router={router} />
-		<ReactQueryDevtools initialIsOpen={false} />
-	</QueryClientProvider>
+	<ReactQueryDevtools initialIsOpen={false} />
 );
