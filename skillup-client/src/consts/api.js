5496318cc1c598dfe91eb8ff2219798713bc320a
Edwin Ortiz <edarcode@gmail.com>
2024-09-15 20:46:24 -0500

    login funcional, falta refresh

--- skillup-client/src/consts/api.js
+++ skillup-client/src/consts/api.js
@@ -2,6 +2,7 @@ class Url {
 	baseUrl = "https://skillup-pi83.onrender.com/api";
 	login = `${this.baseUrl}/login/`;
 	singUp = `${this.baseUrl}/register/`;
+	refreshToken = `${this.baseUrl}/token-refresh/`;
 }
 
 export const API = new Url();
