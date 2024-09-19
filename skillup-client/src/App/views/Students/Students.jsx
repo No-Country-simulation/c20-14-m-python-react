0411890f8662a377230c92de0b0aa397b207d69b
Leonel <leonelgiralde22@gmail.com>
2024-09-19 14:45:55 -0300

    cierre aboutus

--- skillup-client/src/App/views/Students/Students.jsx
+++ skillup-client/src/App/views/Students/Students.jsx
@@ -8,7 +8,6 @@ import Dashboard from "./CursosCompletados/Dashboard";
 import Favorites from "./Favorites";
 import { useState } from "react";
 import CardsData from "./CardsData";
-import About from "../About/About";
 
 function Students() {
 	const [cards, setCards] = useState(CardsData);
@@ -28,7 +27,6 @@ function Students() {
 			<img className="centerBlock" src={imgBg} alt="imagen de fondo codigo" />
 			<Dashboard />
 			<Favorites cards={cards} toggleFavorite={toggleFavorite} />
-			<About />
 		</>
 	);
 }
