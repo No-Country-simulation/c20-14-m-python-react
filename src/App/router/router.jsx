ae336e9a1478c90761599da57233b3bfcbec32ec
Veronica Bravo <bravomv@hotmail.com>
2024-09-20 03:47:16 -0300

    cambio en router y children por cambio de carpeta

--- src/App/router/router.jsx
+++ src/App/router/router.jsx
@@ -1,28 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
-import { COURSE_DETAILS, LOGIN, PAYMENTS, SIGN_UP } from "./children.jsx";
+import {
+    COURSE_DETAILS,
+    LOGIN,
+    MY_COURSES,
+    PAYMENTS,
+    SIGN_UP
+} from "./children.jsx";
 import App from "../App.jsx";
 import NotFound404 from "../views/NotFound404/NotFound404.jsx";
-import { STUDENTS } from "./children.jsx";
 import { INICIO } from "./children.jsx";
 import { USER_PROFILE } from "./children.jsx";
 import { CATALOGUE } from "./children.jsx";
 import { ABOUT } from "./children.jsx";
 
+
 export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		errorElement: <NotFound404 />,
-		children: [
-			SIGN_UP,
-			STUDENTS,
-			INICIO,
-			LOGIN,
-			CATALOGUE,
-			USER_PROFILE,
-			ABOUT,
-			PAYMENTS,
-			COURSE_DETAILS
-		]
-	}
+    {
+        path: "",
+        element: <App />,
+        errorElement: <NotFound404 />,
+        children: [
+            SIGN_UP,
+            MY_COURSES,
+            INICIO,
+            LOGIN,
+            CATALOGUE,
+            USER_PROFILE,
+            ABOUT,
+            PAYMENTS,
+            COURSE_DETAILS
+        ]
+    }
 ]);
+
